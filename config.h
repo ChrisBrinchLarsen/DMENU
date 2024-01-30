fb574bf5eceee37433ae00652ee883bd43ef52c9
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-01-30 15:16:40 +0100

    idk

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static int min_width = 250; 	            /* minimum width when centered        *
 static const unsigned int alpha = 0xA6;     /* Amount of opacity. 0xff is opaque  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"RobotoMono Nerd Font Mono:style=Bold:pixelsize=16:antialias=true:autohint=true",
-	"Fira Code:size=12"
+	"RobotoMono Nerd Font:style=Bold:pixelsize=16:antialias=true:autohint=true",
+	//"Fira Code:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
