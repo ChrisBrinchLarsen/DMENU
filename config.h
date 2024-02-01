4e2231dc9bde5c5acdb3dd6d9adcc89cea3c18dc
ChrisBrinchLarsen <chrisbrinchlarsen@gmail.com>
2024-02-01 14:28:03 +0100

    yikes

--- config.h
+++ config.h
@@ -1,24 +1,24 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
+#import "masterConfig.h"
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 250; 	            /* minimum width when centered        */
-static const unsigned int alpha = 0xA6;     /* Amount of opacity. 0xff is opaque  */
+static int centered = topOrBottom;                    /* -c option; centers dmenu on screen */
+static int min_width = dmenuMinWidth; 	            /* minimum width when centered        */
+static const unsigned int alpha = dmenuAlpha;     /* Amount of opacity. 0xff is opaque  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"RobotoMono Nerd Font:style=Bold:pixelsize=16:antialias=true:autohint=true",
-	//"Fira Code:size=12"
+	dmenuFont,
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                   fg         bg       */
-	[SchemeNorm] = { "#30FFA4", "#031624" },
-	[SchemeSel] = { "#031624", "#30FFA4" },
+	[SchemeNorm] = { mainColorLight, mainColorDark },
+	[SchemeSel] = { mainColorDark, mainColorLight},
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
 	[SchemeNormHighlight]= { "#ffc978", "#222222" },
-	[SchemeOut] = { "#30FFA4", "#044B4A" },
-	[SchemeBorder] = { "#30FFA4", NULL },
+	[SchemeOut] = { mainColorLight, mainColorLessDark },
+	[SchemeBorder] = { mainColorLight, NULL },
 	[SchemeOutHighlight] = { "#ffc978", "#00ffff" },
 };
 
@@ -28,7 +28,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 	[SchemeOut] = { OPAQUE, alpha },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 5;
+static unsigned int lines      = dmenuLines;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -37,5 +37,5 @@ static unsigned int lines      = 5;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = dmenuBorderWidth;
 
